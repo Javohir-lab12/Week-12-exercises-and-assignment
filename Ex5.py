8384b767624120aab0c8d7d684b5d179f2ee49f9
Javohir-lab12 <karimovjavohir0612@gmail.com>
2025-12-16 19:56:01 +0500

    Update Ex5.py

--- Ex5.py
+++ Ex5.py
@@ -10,15 +10,33 @@ ERROR_READING_LINE
 
 with open("votes.txt", "w") as f:
     f.write(data)
-result = {}
+temp_dictionary = {}
+total_votes = 0
 with open("votes.txt", "r") as file:
-    print("OFFICIAL ELECTION RESULTS")
-    print("-------------------------")
     for line in file:
-        if ":" in line:
+        if not ":" in line:
             continue
-        id, name = line.strip().split()
-        if not isinstance(name, str):
+        id, name = line.strip().split(":", 1)
+        name.strip()
+        if not name:
             continue
-        
-
+        if name in temp_dictionary:
+            temp_dictionary[name] += 1
+            total_votes +=1
+        else:
+            temp_dictionary[name] = 1
+            total_votes += 1
+winner = None
+max_vote = 0
+for name, vote in temp_dictionary.items():
+    if vote > max_vote:
+        max_vote = vote
+        winner = name
+with open("result.txt", "w") as result:
+    result.write(f"OFFICIAL ELECTION RESULTS\n")
+    result.write(f"-------------------------\n")
+    for name in temp_dictionary:
+        result.write(f"{name}: {temp_dictionary[name]} votes ({temp_dictionary[name]/total_votes*100:.2f}%)\n")
+    result.write(f"\n-------------------------\n")
+    result.write(f"Total Valid Votes: {total_votes}\n")
+    result.write(f"WINNER: {winner}")
